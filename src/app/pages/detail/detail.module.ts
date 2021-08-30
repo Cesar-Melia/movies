65a88e4263016bf4b1e3585f99a0ff281fd3d606
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-08-30 20:43:00 +0200

    FEAT: page title displayed on header

--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DetailPageRoutingModule } from './detail-routing.module';
 
 import { DetailPage } from './detail.page';
+import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { SharedModule } from '../../shared/shared.module';
     FormsModule,
     IonicModule,
     DetailPageRoutingModule,
+    CoreModule,
     SharedModule,
   ],
   declarations: [DetailPage],
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-// import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    // IonicModule,
+    IonicModule,
     HomePageRoutingModule,
+    CoreModule,
     SharedModule,
   ],
   declarations: [HomePage],
