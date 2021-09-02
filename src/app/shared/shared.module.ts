ba92e5ab320ff554cf0dda6187ae0c8100fc828c
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-09-02 10:35:11 +0200

    FEAT: pipe for format iso languange

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ImgPipe } from './pipes/img.pipe';
+import { LanguagePipe } from './pipes/language.pipe';
 
 @NgModule({
-  declarations: [ImgPipe],
+  declarations: [ImgPipe, LanguagePipe],
   imports: [CommonModule, HttpClientModule],
-  exports: [ImgPipe],
+  exports: [ImgPipe, LanguagePipe],
 })
 export class SharedModule {}
